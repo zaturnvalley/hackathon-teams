0d20dc62dfdca09aff2cb0e0b4a34d39f242f078
zaturnvalley <zacharycmason@gmail.com>
2016-08-11 11:49:59 -0700

    added ability to update and delete team members

--- controllers/teams.js
+++ controllers/teams.js
@@ -25,4 +25,23 @@ router.get('/:name', function(req, res) {
   res.render('teams/show', { team: team });
 });
 
+//New Routes
+router.delete("/:name", function(req, res){
+  teamService.deleteTeam(req.params.name);
+
+  res.send({message: 'Success'});
+});
+
+router.put("/:name", function (req, res){
+  teamService.editTeam(req.params.name, req.body);
+
+  res.send({message: 'success'});
+});
+
+router.get("/:name/edit", function(req, res){
+  var team = teamService.getTeam(req.params.name);
+
+  res.render("teams/edit", {team: team});
+});
+
 module.exports = router;
--- public/js/teams-ajax.js
+++ public/js/teams-ajax.js
@@ -1,3 +1,33 @@
 $(document).ready(function() {
+  $('.delete-link').on('click', function(e){
+    e.preventDefault();
 
+    var teamLink = $(this);
+    var teamUrl = teamLink.attr('href');
+
+    $.ajax({
+      method: 'DELETE',
+      url: teamUrl
+    }).done(function(data){
+      alert('Sad to see you go...');
+      window.location = "/teams";
+    });
+  });
+
+  $('.put-form').on('submit', function(e){
+    e.preventDefault();
+
+    var teamElement = $(this);
+    var teamUrl = teamElement.attr('action');
+    var teamData = teamElement.serialize();
+
+    $.ajax({
+      method: 'PUT',
+      url: teamUrl,
+      data: teamData
+    }).done(function(data){
+      alert('Team successfully edited.');
+      window.location = "/teams";
+    });
+  });
 });
